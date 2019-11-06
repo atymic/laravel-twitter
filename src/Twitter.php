392853ae5737f560a48076420091cdbc5e23373f
atymic <atymicq@gmail.com>
2019-11-06 14:50:08 +1100

    feat: extract helpers

--- src/Twitter.php
+++ src/Twitter.php
@@ -6,6 +6,7 @@ use Atymic\Twitter\Traits\AccountTrait;
 use Atymic\Twitter\Traits\BlockTrait;
 use Atymic\Twitter\Traits\DirectMessageTrait;
 use Atymic\Twitter\Traits\FavoriteTrait;
+use Atymic\Twitter\Traits\FormattingHelpers;
 use Atymic\Twitter\Traits\FriendshipTrait;
 use Atymic\Twitter\Traits\GeoTrait;
 use Atymic\Twitter\Traits\HelpTrait;
@@ -29,7 +30,8 @@ class Twitter
 {
     const VERSION = '3.x-dev';
 
-    use AccountTrait,
+    use FormattingHelpers,
+        AccountTrait,
         BlockTrait,
         DirectMessageTrait,
         FavoriteTrait,
@@ -257,113 +259,6 @@ class Twitter
     public function post($name, $parameters = [], $multipart = false)
     {
         return $this->query($name, 'POST', $parameters, $multipart);
-    }
-
-    public function linkify($tweet)
-    {
-        if (is_object($tweet)) {
-            $type = 'object';
-            $tweet = $this->jsonDecode(json_encode($tweet), true);
-        } elseif (is_array($tweet)) {
-            $type = 'array';
-        } else {
-            $type = 'text';
-            $text = ' ' . $tweet;
-        }
-
-        $patterns = [];
-        $patterns['url'] = '(?xi)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
-        $patterns['mailto'] = '([_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3}))';
-        $patterns['user'] = ' +@([a-z0-9_]*)?';
-        $patterns['hashtag'] = '(?:(?<=\s)|^)#(\w*[\p{L}\-\d\p{Cyrillic}\d]+\w*)';
-        $patterns['long_url'] = '>(([[:alnum:]]+:\/\/)|www\.)?([^[:space:]]{12,22})([^[:space:]]*)([^[:space:]]{12,22})([[:alnum:]#?\/&=])<';
-
-        if ($type == 'text') {
-            // URL
-            $pattern = '(?xi)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
-            $text = preg_replace_callback('#' . $patterns['url'] . '#i', function ($matches) {
-                $input = $matches[0];
-                $url = preg_match('!^https?://!i', $input) ? $input : "http://$input";
-
-                return '<a href="' . $url . '" target="_blank" rel="nofollow">' . "$input</a>";
-            }, $text);
-        } else {
-            $text = $tweet['text'];
-            $entities = $tweet['entities'];
-
-            $search = [];
-            $replace = [];
-
-            if (array_key_exists('media', $entities)) {
-                foreach ($entities['media'] as $media) {
-                    $search[] = $media['url'];
-                    $replace[] = '<a href="' . $media['media_url_https'] . '" target="_blank">' . $media['display_url'] . '</a>';
-                }
-            }
-
-            if (array_key_exists('urls', $entities)) {
-                foreach ($entities['urls'] as $url) {
-                    $search[] = $url['url'];
-                    $replace[] = '<a href="' . $url['expanded_url'] . '" target="_blank" rel="nofollow">' . $url['display_url'] . '</a>';
-                }
-            }
-
-            $text = str_replace($search, $replace, $text);
-        }
-
-        // Mailto
-        $text = preg_replace('/' . $patterns['mailto'] . '/i', '<a href="mailto:\\1">\\1</a>', $text);
-
-        // User
-        $text = preg_replace('/' . $patterns['user'] . '/i', ' <a href="https://twitter.com/\\1" target="_blank">@\\1</a>', $text);
-
-        // Hashtag
-        $text = preg_replace('/' . $patterns['hashtag'] . '/ui', '<a href="https://twitter.com/search?q=%23\\1" target="_blank">#\\1</a>', $text);
-
-        // Long URL
-        $text = preg_replace('/' . $patterns['long_url'] . '/', '>\\3...\\5\\6<', $text);
-
-        // Remove multiple spaces
-        $text = preg_replace('/\s+/', ' ', $text);
-
-        return trim($text);
-    }
-
-    public function ago($timestamp)
-    {
-        if (is_numeric($timestamp) && (int) $timestamp == $timestamp) {
-            $carbon = Carbon::createFromTimeStamp($timestamp);
-        } else {
-            $dt = new \DateTime($timestamp);
-            $carbon = Carbon::instance($dt);
-        }
-
-        return $carbon->diffForHumans();
-    }
-
-    public function linkUser($user)
-    {
-        return 'https://twitter.com/' . (is_object($user) ? $user->screen_name : $user);
-    }
-
-    public function linkTweet($tweet)
-    {
-        return $this->linkUser($tweet->user) . '/status/' . $tweet->id_str;
-    }
-
-    public function linkRetweet($tweet)
-    {
-        return 'https://twitter.com/intent/retweet?tweet_id=' . $tweet->id_str;
-    }
-
-    public function linkAddTweetToFavorites($tweet)
-    {
-        return 'https://twitter.com/intent/favorite?tweet_id=' . $tweet->id_str;
-    }
-
-    public function linkReply($tweet)
-    {
-        return 'https://twitter.com/intent/tweet?in_reply_to=' . $tweet->id_str;
     }
 
     public function error()
